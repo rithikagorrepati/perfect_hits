d0a488ced069aa9b32fd7c00538db60616ce04bd
rithikagorrepati <rithika.gorrepati@gmail.com>
2024-09-09 22:35:42 -0400

    Finalize CRISPR spacer extraction script and environment

--- rgorrepati7_11.sh
+++ rgorrepati7_11.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Check if correct number of arguments are provided
 if [ $# -ne 2 ]; then
     echo "Usage: $0 <assembly.fasta> <repeat.fasta>"
     exit 1
@@ -9,11 +8,9 @@ fi
 assembly=$1
 repeat=$2
 
-# Find perfect matches
 blastn -query $repeat -subject $assembly -task blastn-short -outfmt 6 | \
     awk '$3==100 && $4==length($1)' > perfect_matches.txt
 
-# Extract spacer coordinates
 awk 'BEGIN {OFS="\t"} 
      NR>1 {
         start = $9 + $4
@@ -22,7 +19,6 @@ awk 'BEGIN {OFS="\t"}
             print $2, start, end
      }' perfect_matches.txt > spacers.bed
 
-# Extract spacer sequences
 seqtk subseq $assembly spacers.bed > spacers.fasta
 
 echo "Spacer sequences extracted to spacers.fasta"
